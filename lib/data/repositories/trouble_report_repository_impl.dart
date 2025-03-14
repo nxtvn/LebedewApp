4e741fa23a421eb3c5356c45a453cf11452c2a44
schererj <schererjulian@yahoo.de>
2025-03-14 18:08:03 +0100

    final fix

--- lib/data/repositories/trouble_report_repository_impl.dart
+++ lib/data/repositories/trouble_report_repository_impl.dart
@@ -6,12 +6,10 @@ import '../../domain/entities/trouble_report.dart';
 import '../../domain/services/email_service.dart';
 import '../../domain/services/image_storage_service.dart';
 import '../../core/utils/image_utils.dart';
-import 'package:intl/intl.dart';
 
 class TroubleReportRepositoryImpl implements TroubleReportRepository {
   final EmailService _emailService;
   final ImageStorageService _imageStorageService;
-  final DateFormat _dateFormatter = DateFormat('dd.MM.yyyy');
 
   TroubleReportRepositoryImpl(this._emailService, this._imageStorageService);
 
@@ -106,64 +104,4 @@ class TroubleReportRepositoryImpl implements TroubleReportRepository {
     }
   }
 
-  String _createEmailBody(TroubleReport report) {
-    final buffer = StringBuffer();
-    
-    buffer.writeln('<h2>Neue Störungsmeldung</h2>');
-    buffer.writeln('<p><strong>Art des Anliegens:</strong> ${report.type.label}</p>');
-    buffer.writeln('<p><strong>Dringlichkeit:</strong> ${report.urgencyLevel.label}</p>');
-    
-    buffer.writeln('<h3>Kontaktdaten</h3>');
-    buffer.writeln('<p><strong>Name:</strong> ${report.name}</p>');
-    buffer.writeln('<p><strong>E-Mail:</strong> ${report.email}</p>');
-    
-    if (report.phone != null && report.phone!.isNotEmpty) {
-      buffer.writeln('<p><strong>Telefon:</strong> ${report.phone}</p>');
-    }
-    
-    if (report.address != null && report.address!.isNotEmpty) {
-      buffer.writeln('<p><strong>Adresse:</strong> ${report.address}</p>');
-    }
-    
-    buffer.writeln('<h3>Gerätedaten</h3>');
-    
-    if (report.deviceModel != null && report.deviceModel!.isNotEmpty) {
-      buffer.writeln('<p><strong>Gerätemodell:</strong> ${report.deviceModel}</p>');
-    }
-    
-    if (report.manufacturer != null && report.manufacturer!.isNotEmpty) {
-      buffer.writeln('<p><strong>Hersteller:</strong> ${report.manufacturer}</p>');
-    }
-    
-    if (report.serialNumber != null && report.serialNumber!.isNotEmpty) {
-      buffer.writeln('<p><strong>Seriennummer:</strong> ${report.serialNumber}</p>');
-    }
-    
-    if (report.errorCode != null && report.errorCode!.isNotEmpty) {
-      buffer.writeln('<p><strong>Fehlercode:</strong> ${report.errorCode}</p>');
-    }
-    
-    if (report.occurrenceDate != null) {
-      buffer.writeln('<p><strong>Datum des Vorfalls:</strong> ${_dateFormatter.format(report.occurrenceDate!)}</p>');
-    }
-    
-    if (report.serviceHistory != null && report.serviceHistory!.isNotEmpty) {
-      buffer.writeln('<p><strong>Servicehistorie:</strong> ${report.serviceHistory}</p>');
-    }
-    
-    buffer.writeln('<h3>Problembeschreibung</h3>');
-    buffer.writeln('<p>${report.description}</p>');
-    
-    if (report.energySources.isNotEmpty) {
-      buffer.writeln('<p><strong>Energiequellen:</strong> ${report.energySources.join(', ')}</p>');
-    }
-    
-    buffer.writeln('<p><strong>Wartungsvertrag:</strong> ${report.hasMaintenanceContract ? 'Ja' : 'Nein'}</p>');
-    
-    if (report.imagesPaths.isNotEmpty) {
-      buffer.writeln('<p><strong>Anzahl der Bilder:</strong> ${report.imagesPaths.length}</p>');
-    }
-    
-    return buffer.toString();
-  }
 } 
